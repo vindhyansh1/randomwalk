82b7898f73e2e0283ae53aa108f88b90cae859fc
Vindhyansh Mall <m.vindhyansh@gmail.com>
2020-03-01 11:37:19 +0530

    Add files via upload

---
+++ create_nework2.py
@@ -0,0 +1,98 @@
+import re
+import numpy as np
+
+
+adj_list = [[] for i in range(4039)]
+gender_list = [77]*4039
+gender_dist = dict()
+gender_wise_neighbour_indices = []
+
+def fillupmat(nodeid):
+    circle_file = open(str(nodeid)+".circles",'r')
+    y=[]
+    for circle in circle_file:
+        x=re.split('[ \t]+',circle)
+        y+=[int(a) for a in x[1:-1]]
+
+    for i in y:
+        adj_list[nodeid].append(i)
+        adj_list[i].append(nodeid)
+    circle_file.close()
+
+    edge_file = open(str(nodeid)+'.edges','r')
+    for edge in edge_file:
+        vertices = edge.split(' ')
+        adj_list[int(vertices[0])].append(int(vertices[1]))
+        adj_list[int(vertices[1])].append(int(vertices[0]))
+    edge_file.close()
+
+    #assigning gender
+    feature_file = open(str(nodeid)+'.feat','r')
+    feature_names_file = open(str(nodeid)+'.feat','r')
+    gender_index = 77
+    for line in feature_names_file:
+        if line.split(' ')[1].split(';')[0] == 'gender':
+            gender_index = line.split(' ')[0]
+            break
+    
+    count_gender77 = 0
+    count_gender78 = 0
+    for row in feature_file:
+        feature_data = row.split(' ')
+        nodeid = feature_data[0]
+        if row[gender_index+1] == 1:
+            gender_list[nodeid] = 77
+            count_gender77+=1
+        else:
+            gender_list[nodeid] = 78
+            count_gender78 += 1
+    
+    gender_dist[nodeid] = (count_gender77,count_gender78)
+
+def get_gender_wise_neighbours(adj_list):
+    for nodeid in range(len(adj_list)):
+        gender77_list = []
+        gender78_list = []
+        for neighbourid in adj_list:
+            if(gender_list[neighbourid]==77):
+                gender77_list.append(neighbourid)
+            else:
+                gender78_list.append(neighbourid)
+        gender_wise_neighbour_indices.append((gender77_list,gender78_list)) 
+
+def fairwalk(adj_list,walk_len,walk_num):
+    fairwalk_traces = []
+    for nodeid in range(len(adj_list)):
+        traces = []
+        for j in range(walk_num):
+            walk = []
+            current_node = nodeid
+            for step in range(walk_len):
+                prob_77 = gender_dist[current_node][0]/(gender_dist[current_node][0] + gender_dist[current_node][1])
+                r = np.random.random()
+                try:
+                    if r<=prob_77:
+                        group77_neighbours = gender_wise_neighbour_indices[current_node][0]
+                        random_neighbor = np.random.choice(group77_neighbours)
+                    else:
+                        group78_neighbours = gender_wise_neighbour_indices[current_node][1]
+                        random_neighbor = np.random.choice(group78_neighbours)
+                except:
+                    random_neighbor = 0
+                walk.append(adj_list[current_node][random_neighbor])
+                current_node = adj_list[current_node][random_neighbor]
+            traces.append(walk)
+        fairwalk_traces.append(traces)
+    return fairwalk_traces
+
+def main():
+    egos = [0,107,348,414,686,698,1684,1912,3437,3980]
+    for ego_node in egos:
+        fillupmat(ego_node)
+    
+    get_gender_wise_neighbours(adj_list)
+    fairwalk(adj_list,5,1)
+    
+    
+if __name__ == '__main__':
+    main()
